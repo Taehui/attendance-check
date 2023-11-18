df28a806be84f0020ca8b5b67f58a538a430933f
SK텔레콤 홍태희 <taehui.hong@sk.com>
2023-11-18 16:48:51 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+# from langchain.chat_loaders import ChatOpenAI
+# from langchain.llms import OpenAI
+# from dotenv import load_dotenv
+# load_dotenv()
+
+st.title('어린이영어예배부 출석체크앱')
